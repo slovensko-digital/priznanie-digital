8563eca6b7a1e9856854695c3f0427efddbb4f2b
Július Retzer <jul.retzer@gmail.com>
2020-02-18 15:05:27 +0100

    Initial commit

--- __tests__/snapshot.js
+++ __tests__/snapshot.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import renderer from 'react-test-renderer'
-import Index from '../pages/index'
+import React from "react";
+import renderer from "react-test-renderer";
+import Index from "../pages/index";
 
-it('renders homepage unchanged', () => {
-  const tree = renderer.create(<Index />).toJSON()
-  expect(tree).toMatchSnapshot()
-})
+// it('renders homepage unchanged', () => {
+//   const tree = renderer.create(<Index />).toJSON()
+//   expect(tree).toMatchSnapshot()
+// })
