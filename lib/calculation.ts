0d62be1c8f1f5b9ce1473bd46407a26bb8e1041a
Július Retzer <jul.retzer@gmail.com>
2020-02-20 00:08:04 +0100

    Remove now unnecesssary things from calc

--- lib/calculation.ts
+++ lib/calculation.ts
@@ -1,34 +1,6 @@
-const PAUSALNE_VYDAVKY = 0.6;
-
-function calculateRows(tf: TaxForm) {
-  tf.r008 = tf.r009 + tf.income;
-  tf.expense = tf.income * 0.6; // TODO max 20 000
-
-  tf.r105 = tf.r081;
-
-  tf = calculateTax(tf);
-
-  return tf;
-}
-
-function calculateTax(tf: TaxForm) {
-  const tax = tf.r105;
-  // - tf.r106 +
-  // tf.r108 +
-  // tf.r110 -
-  // tf.r112 +
-  // tf.r114 +
-  // tf.r116 +
-  // tf.r117 -
-  // tf.r118 -
-  // tf.r119 -
-  // tf.r120 -
-  // tf.r121 -
-  // tf.r122 -
-  // tf.r123 -
-  // tf.r124;
-
-  tf.r125 = tax;
+function calculate(tf: TaxForm) {
+  const flatrate_expenses = tf.income * 0.6;
+  tf.expense = flatrate_expenses < 20000 ? flatrate_expenses : 20000;
 
   return tf;
 }
@@ -38,6 +10,5 @@ function summary(tf: TaxForm) {
     "Základ dane": tf.r080,
     Daň: tf.r105,
     "Daň na úhradu": tf.r125
-    // "Daňový preplatok": tf.r126
   };
 }
