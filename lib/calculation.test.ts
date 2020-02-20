742c4957fc2d950d91609646834293abbb9bd1c8
Július Retzer <jul.retzer@gmail.com>
2020-02-20 14:59:14 +0100

    Dan

--- lib/calculation.test.ts
+++ lib/calculation.test.ts
@@ -1,26 +1,42 @@
 import { calculate } from "./calculation";
 import { TaxForm, TaxFormUserInput } from "./types";
 
-test("Should calculate r080", () => {
-  const input: TaxFormUserInput = {
-    t1r10_prijmy: 20000,
-    t1r10_vydavky: 12000,
+describe("r080_zaklad_dane", () => {
+  test("Should calculate r080", () => {
+    const input: TaxFormUserInput = {
+      t1r10_prijmy: 20000,
+      t1r10_vydavky: 12000,
 
-    priloha3_r11_socialne: 0,
-    priloha3_r13_zdravotne: 0
-  };
-  const result = calculate(input);
-  expect(result.r080_zaklad_dane).toBe(4062.65);
+      priloha3_r11_socialne: 0,
+      priloha3_r13_zdravotne: 0
+    };
+    const result = calculate(input);
+    expect(result.r080_zaklad_dane).toBe(4062.65);
+  });
+
+  test("Should calculate r080 with insurance deduction", () => {
+    const input: TaxFormUserInput = {
+      t1r10_prijmy: 20000,
+      t1r10_vydavky: 12000,
+
+      priloha3_r11_socialne: 1000,
+      priloha3_r13_zdravotne: 1000
+    };
+    const result = calculate(input);
+    expect(result.r080_zaklad_dane).toBe(2062.65);
+  });
 });
 
-test("Should calculate r080 with insurance deduction", () => {
-  const input: TaxFormUserInput = {
-    t1r10_prijmy: 20000,
-    t1r10_vydavky: 12000,
+describe("r105_dan", () => {
+  test("Should calculate r080", () => {
+    const input: TaxFormUserInput = {
+      t1r10_prijmy: 20000,
+      t1r10_vydavky: 12000,
 
-    priloha3_r11_socialne: 1000,
-    priloha3_r13_zdravotne: 1000
-  };
-  const result = calculate(input);
-  expect(result.r080_zaklad_dane).toBe(2062.65);
+      priloha3_r11_socialne: 1000,
+      priloha3_r13_zdravotne: 1000
+    };
+    const result = calculate(input);
+    expect(result.r105_dan).toBe(391.9);
+  });
 });
--- lib/calculation.ts
+++ lib/calculation.ts
@@ -1,14 +1,16 @@
 import { TaxForm, TaxFormUserInput } from "./types";
+import { roundToTwoDecimals } from "./helpers";
 
-const nezdanitelnaCastZakladu = 3937.35;
-const flatrateExpensesMax = 20000;
+const NEZDANITELNA_CAST_ZAKLADU = 3937.35;
+const PAUSALNE_VYDAVKY_MAX = 20000;
+const DAN_Z_PRIJMU_SADZBA = 0.19;
 
 export function calculate(taxFormUserInput: TaxFormUserInput) {
   // Make a copy of taxFormUserInput
   const tf: TaxForm = Object.assign({}, taxFormUserInput);
 
   const flatrateExpenses = tf.t1r10_prijmy * 0.6;
-  tf.t1r10_vydavky = Math.min(flatrateExpenses, flatrateExpensesMax);
+  tf.t1r10_vydavky = Math.min(flatrateExpenses, PAUSALNE_VYDAVKY_MAX);
 
   // wow toto je uplne sialena ezoterika :D ale tak je definovana business logika, ale tak urcite sa to da zjednodusit
   tf.r041 = tf.t1r10_prijmy;
@@ -18,13 +20,15 @@ export function calculate(taxFormUserInput: TaxFormUserInput) {
   tf.r047 = tf.r043; // tf.r044 + tf.r045 - tf.r046);
   tf.r055 = tf.r047;
   tf.r057 = tf.r055;
-  tf.r073 = nezdanitelnaCastZakladu;
+  tf.r073 = NEZDANITELNA_CAST_ZAKLADU;
   tf.r072 = tf.r057; // + tf.r040;
   tf.r077 = tf.r073; // + tf.r074 + tf.r075 + tf.r076;
   tf.r078 = Math.max(tf.r072 - tf.r077, 0);
 
   tf.r080_zaklad_dane = tf.r078; //+ tf.r065 + tf.r071 + tf.r079)
 
+  tf.r081 = roundToTwoDecimals(tf.r080_zaklad_dane * DAN_Z_PRIJMU_SADZBA);
+  tf.r105_dan = tf.r081;
   return tf;
 }
 
