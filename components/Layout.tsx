198ffb36c13a942e1824467c4108a07ca3acf34e
Július Retzer <jul.retzer@gmail.com>
2020-02-21 20:14:01 +0100

    Title is a link to home

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,13 +2,16 @@ import React from "react";
 
 import styles from "./layout.module.css";
 import Head from "next/head";
+import Link from "next/link";
 
 export default ({ children }) => (
   <div className={styles.container}>
     <Head>
       <title>priznanie.digital</title>
     </Head>
-    <h1 className={styles.title}>priznanie.digital</h1>
+    <Link href="/">
+      <h1 className={styles.title}>priznanie.digital</h1>
+    </Link>
     {children}
   </div>
 );
