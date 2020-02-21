b37cc9f50b0c4d16cb85790e72823fb70a7da2cc
Július Retzer <jul.retzer@gmail.com>
2020-02-22 00:12:24 +0100

    Prevent FOUC

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,10 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
   render() {
     return (
       <Html>
