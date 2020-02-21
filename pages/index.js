d172558df63df4c83361c5a2d1d4a6a6f322f7af
Martin Starosta <martin.starosta83@gmail.com>
2020-02-21 13:53:07 +0100

    Adds navody-frontend framework.

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
-import Head from "next/head";
-import React from "react";
-import styles from "./index.module.css";
-import MainForm from "../components/MainForm";
+import Head from 'next/head'
+import React from 'react'
+import styles from './index.module.css'
+import MainForm from '../components/MainForm'
 
 const Home = () => (
   <div className={styles.container}>
     <Head>
       <title>priznanie.digital</title>
-      <link rel="icon" href="/favicon.ico" />
+      <link rel='icon' href='/favicon.ico' />
+
+      <link rel='stylesheet' href='/stylesheets/libs.css'></link>
+      <link
+        rel='stylesheet'
+        href='/stylesheets/navody-digital-0.1.8.min.css'
+      ></link>
     </Head>
 
     <main>
@@ -16,7 +22,11 @@ const Home = () => (
     </main>
 
     <footer></footer>
-  </div>
-);
 
-export default Home;
+    <script src='javascript/libs.js'></script>
+    <script src='javascript/navody-digital-0.1.8.min.js'></script>
+    <script>window.GOVUKFrontend.initAll()</script>
+  </div>
+)
+
+export default Home
