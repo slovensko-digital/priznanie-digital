10fc86464044d6f6ebc27833ea26943407e3d11e
Július Retzer <jul.retzer@gmail.com>
2020-02-22 14:26:14 +0100

    Osobne udaje

--- pages/osobne-udaje.tsx
+++ pages/osobne-udaje.tsx
@@ -3,6 +3,7 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import Link from "next/link";
 import { personalInformationUserInputInitialValues } from "../lib/initialValues";
 import { useRouter } from "next/router";
+import { Input } from "../components/FormComponents";
 
 const nextUrl = "/vysledky";
 
@@ -23,41 +24,33 @@ export default ({ taxForm, updateTaxForm }) => {
       }}
       onSubmit={handleSubmit}
     >
-      {() => (
-        <Form className="form">
-          <h2>Údaje o daňovníkovi</h2>
-          <label htmlFor="r001_dic">DIČ</label>
-          <Field name="r001_dic" type="text" />
+      <Form className="form">
+        <h2>Údaje o daňovníkovi</h2>
 
-          <label htmlFor="r002_datum_narodenia">Dátum narodenia</label>
-          <Field name="r002_datum_narodenia" type="text" />
+        <Input name="r001_dic" type="text" label="DIČ" />
+        <Input
+          name="r002_datum_narodenia"
+          type="text"
+          label="Dátum narodenia"
+        />
+        <Input name="r005_meno" type="text" label="Meno" />
+        <Input name="r004_priezvisko" type="text" label="Priezvisko" />
 
-          <label htmlFor="r005_meno">Meno</label>
-          <Field name="r005_meno" type="text" />
-          <ErrorMessage name="r005_meno" />
+        <h3>Adresa trvalého pobytu</h3>
+        <Input name="r007_ulica" type="text" label="Ulica" />
+        <Input name="r008_cislo" type="text" label="Súpisné/orientačné číslo" />
+        <Input name="r009_psc" type="text" label="PSČ" />
+        <Input name="r010_mesto" type="text" label="Mesto" />
+        <Input name="r011_stat" type="text" label="Štát" />
+        <Input name="r007_ulica" type="text" label="Ulica" />
 
-          <label htmlFor="r004_priezvisko">Priezvisko</label>
-          <Field name="r004_priezvisko" type="text" />
-          <ErrorMessage name="r004_priezvisko" />
-
-          <h3>Adresa trvalého pobytu</h3>
-          <label htmlFor="r007_ulica">Ulica</label>
-          <Field name="r007_ulica" type="text" />
-          <label htmlFor="r008_cislo">Súpisné/orientačné číslo</label>
-          <Field name="r008_cislo" type="text" />
-          <label htmlFor="r009_psc">PSČ</label>
-          <Field name="r009_psc" type="text" />
-          <label htmlFor="r010_mesto">Mesto</label>
-          <Field name="r010_mesto" type="text" />
-          <label htmlFor="r011_stat">Štát</label>
-          <Field name="r011_stat" type="text" />
-          <label htmlFor="t1r10_prijmy">Prijem</label>
-          <Link href="/partner">
-            <button className="govuk-button">Back</button>
-          </Link>
-          <button className="govuk-button" type="submit">Next</button>
-        </Form>
-      )}
+        <Link href="/partner">
+          <button className="govuk-button">Back</button>
+        </Link>
+        <button className="govuk-button" type="submit">
+          Next
+        </button>
+      </Form>
     </Formik>
   );
 };
