7377deceee89ac18914ff5628f7331a22df03202
Július Retzer <jul.retzer@gmail.com>
2020-02-22 16:27:09 +0100

    Vysledky

--- pages/vysledky.tsx
+++ pages/vysledky.tsx
@@ -9,12 +9,30 @@ export default ({ taxForm }) => (
       <a className="govuk-back-link">Naspat</a>
     </Link>
     <h2>Vysledky</h2>
-    <div className={styles.summary}>
-      <div>Základ dane: {taxForm.r080_zaklad_dane_celkovo} </div>
-      <div>Daň: {taxForm.r105_dan}</div>
-      <div>Daň na úhradu: {taxForm.r125_dan_na_uhradu}</div>
-      <div>Daňový preplatok: {taxForm.r126_danovy_preplatok}</div>
-    </div>
+    <dl className="govuk-summary-list">
+      <div className="govuk-summary-list__row">
+        <dt className="govuk-summary-list__key">Základ dane</dt>
+        <dd className="govuk-summary-list__value">
+          {taxForm.r080_zaklad_dane_celkovo}
+        </dd>
+      </div>
+      <div className="govuk-summary-list__row">
+        <dt className="govuk-summary-list__key">Daň</dt>
+        <dd className="govuk-summary-list__value">{taxForm.r105_dan}</dd>
+      </div>
+      <div className="govuk-summary-list__row">
+        <dt className="govuk-summary-list__key">Daň na úhradu</dt>
+        <dd className="govuk-summary-list__value">
+          {taxForm.r125_dan_na_uhradu}
+        </dd>
+      </div>
+      <div className="govuk-summary-list__row">
+        <dt className="govuk-summary-list__key">Daňový preplatok</dt>
+        <dd className="govuk-summary-list__value">
+          {taxForm.r126_danovy_preplatok}
+        </dd>
+      </div>
+    </dl>
     <button className="govuk-button">Stiahnut XML (TODO)</button>
   </>
 );
