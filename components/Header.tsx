7830507ce2aeb295922995be043b3f7a39038fb9
Július Retzer <jul.retzer@gmail.com>
2020-02-22 13:18:25 +0100

    Footer and header

---
+++ components/Header.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header
+      className="sdn-header sdn-header-1200 "
+      role="banner"
+      data-module="sdn-header"
+    >
+      <div className="sdn-header__container govuk-width-container ">
+        <a href="/" className="sdn-header__link">
+          <span className="sdn-header__product-name">priznanie.digital</span>
+        </a>
+
+        <div className="sdn-header__content">
+          <button
+            type="button"
+            role="button"
+            className="sdn-header__menu-button sdn-header__menu-button js-header-toggle"
+            aria-controls="navigation"
+            aria-label="Show or hide Top Level Navigation"
+          >
+            Menu
+          </button>
+          <nav>
+            <ul
+              id="navigation"
+              className="sdn-header__navigation "
+              aria-label="Top Level Navigation"
+            >
+              <li>
+                <a className="sdn-header__link " href="#1">
+                  Časté otázky
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,15 +3,22 @@ import React from "react";
 import styles from "./Layout.module.css";
 import Head from "next/head";
 import Link from "next/link";
+import Header from "./Header";
+import Footer from "./Footer";
 
 export default ({ children }) => (
-  <div className={styles.container}>
-    <Head>
-      <title>priznanie.digital</title>
-    </Head>
-    <Link href="/">
-      <h1 className={styles.title}>priznanie.digital</h1>
-    </Link>
-    {children}
-  </div>
+  <>
+    <Header />
+    <div className={styles.container}>
+      <Head>
+        <title>priznanie.digital</title>
+      </Head>
+
+      <Link href="/">
+        <h1 className={styles.title}>priznanie.digital</h1>
+      </Link>
+      {children}
+    </div>
+    <Footer />
+  </>
 );
