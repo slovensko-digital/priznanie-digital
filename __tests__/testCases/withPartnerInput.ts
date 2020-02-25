6d0f83215353cfd0a96a3b7581e8c16dca22505d
Július Retzer <jul.retzer@gmail.com>
2020-02-25 15:11:02 +0100

    Create with partner input

---
+++ __tests__/testCases/withPartnerInput.ts
@@ -0,0 +1,17 @@
+import { TaxFormUserInput } from "../../lib/types";
+
+export const withPartnerTaxForm: TaxFormUserInput = {
+  t1r10_prijmy: 20000,
+  priloha3_r11_socialne: 0,
+  priloha3_r13_zdravotne: 0,
+  r001_dic: "111111111",
+  r002_datum_narodenia: "22.02.1993",
+  r003_nace: "62232",
+  r004_priezvisko: "Retzer",
+  r005_meno: "Julius",
+  r007_ulica: "Kastielska",
+  r008_cislo: "13",
+  r009_psc: "82105",
+  r010_obec: "Bratislava",
+  r011_stat: "Slovensko",
+};
