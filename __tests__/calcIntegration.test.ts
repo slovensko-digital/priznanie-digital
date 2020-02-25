ff6fa5ce4cbdb7e11b0898f76595b228c7d0ebc2
Július Retzer <jul.retzer@gmail.com>
2020-02-25 16:30:13 +0100

    Add correct test cases

--- __tests__/calcIntegration.test.ts
+++ __tests__/calcIntegration.test.ts
@@ -1,16 +1,20 @@
-import { TaxFormUserInput } from "../lib/types";
-import { withPartnerTaxForm } from "./testCases/withPartnerTaxForm";
+import { withPartnerInput } from "./testCases/withPartnerInput";
 import { convertToXML, convertToJson } from "../lib/xml/xmlConverter";
 import { xml2json } from "xml-js";
 // @ts-ignore
 import * as withPartnerXML from "./testCases/withPartner.xml";
 import { promises as fs } from "fs";
-import { OutputJson } from "../lib/xml/OutputJson";
+import { calculate } from "../lib/calculation";
 
 const comparable = (xml: string) => xml2json(xml, { compact: true, spaces: 2 });
 const stringify = (object: object) => JSON.stringify(object, null, 2);
 test("withPartner", () => {
-  const taxForm = withPartnerTaxForm;
+  const taxForm = calculate(withPartnerInput);
+  fs.writeFile(
+    __dirname + "/testCases/withPartnerTaxForm.output.json",
+    stringify(taxForm),
+  );
+
   const outputXml = convertToXML(taxForm);
   const outputJson = convertToJson(taxForm);
 
