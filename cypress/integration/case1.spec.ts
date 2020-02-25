e1c076c7f378e805e4536e5e93eda277e2962906
Július Retzer <jul.retzer@gmail.com>
2020-02-25 19:53:39 +0100

    Add TS to cypress

---
+++ cypress/integration/case1.spec.ts
@@ -0,0 +1,12 @@
+/// <reference types="Cypress" />
+import { withPartnerInput } from "../../__tests__/testCases/withPartnerInput";
+
+describe("The Home Page", function() {
+  it("successfully loads", function() {
+    cy.visit("/");
+    cy.contains("Suhlasím").click();
+    cy.get('input[name="t1r10_prijmy"]').type(
+      withPartnerInput.t1r10_prijmy?.toString() as string,
+    );
+  });
+});
