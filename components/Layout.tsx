9448df0caa04d07ae87de3f5eae763b98a2666df
Július Retzer <jul.retzer@gmail.com>
2020-02-27 22:16:56 +0100

    Alpha preklad

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -25,14 +25,10 @@ const Layout = ({ children, debug }) => (
       <div className="govuk-phase-banner">
         <p className="govuk-phase-banner__content">
           <strong className="govuk-tag govuk-phase-banner__content__tag">
-            alpha
+            Alpha
           </strong>
           <span className="govuk-phase-banner__text">
-            This is a new service – your{" "}
-            <a className="govuk-link" href="#">
-              feedback
-            </a>{" "}
-            will help us to improve it.
+            Táto služba je vo vývoji.
           </span>
         </p>
       </div>
