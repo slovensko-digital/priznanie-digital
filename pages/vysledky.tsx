22d711121e3fc6a5650e033adda659a4af6aaa73
Július Retzer <jul.retzer@gmail.com>
2020-02-27 18:27:53 +0100

    Vysledky list

--- pages/vysledky.tsx
+++ pages/vysledky.tsx
@@ -42,6 +42,45 @@ const Vysledky = ({ taxForm }) => {
       <button className="govuk-button" onClick={onExport}>
         Stiahnut XML
       </button>
+      <div>
+        <ol>
+          <li>
+            Prihláste sa na portál Finančnej správy - buď pomocou Identifikátora
+            a hesla, Kvalifikovaného elektronického podpisu (KEP) alebo
+            Občianskym preukazom s elektronickým čipom (eID)
+          </li>
+          <li>Kliknite na Katálog formulárov.</li>
+          <li>
+            Nájdite kolonku “Daň z príjmov fyzickej osoby” (vyznačená modrou) a
+            kliknite.
+          </li>
+          <li>
+            Kliknite na kolonku “Ďanové priznanie k dani z príjmov fyzickej
+            osoby (typ B) a kliknite.
+          </li>
+          <li>
+            Kliknite na ikonku “zošita s ceruzkou” pri kolonke “Daňové priznanie
+            k dani z príjmov FO (typ B) za obdobie 2019.
+          </li>
+          <li>Kliknite na kolonku “Načítať zo súboru” vľavo nižšie. </li>
+          <li>Potvrďte “Áno”.</li>
+          <li>
+            Vyhľadajte súbor XML (pravdepodobne v adresári “Stiahnuté”) a
+            označte ho.
+          </li>
+          <li>
+            Kliknite na kolonku “Podpísať KEP-om” a následne “Podať podpísane
+            KEP-om” alebo “Podpísať a podať EZ-ou”, ak ste na príhlasenie
+            použili identifikátor a heslo.
+          </li>
+          <li>
+            Vráťte sa na Hlavnú stránku a v sekcii “História komunikácie”
+            skontrolujte v “Odoslané dokumenty”, že ste daňové priznanie úspešne
+            podali. V “Stave spracovania” by ste mali mať napísané “Prijaté a
+            potvrdené”.
+          </li>
+        </ol>
+      </div>
     </>
   );
 };
