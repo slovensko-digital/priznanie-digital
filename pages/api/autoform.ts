dd5ea1b9ffb6ec31f5e4b8bc53d4a1af9a7190be
Július Retzer <jul.retzer@gmail.com>
2020-02-28 12:53:50 +0100

    Only active

--- pages/api/autoform.ts
+++ pages/api/autoform.ts
@@ -4,7 +4,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   res.statusCode = 200;
   const personsDataJson = await fetch(
-    `https://autoform.ekosystem.slovensko.digital/api/corporate_bodies/search?q=name:${req.query.fullName}&limit=20&private_access_token=${process.env.autoformtoken}
+    `https://autoform.ekosystem.slovensko.digital/api/corporate_bodies/search?q=name:${req.query.fullName}&limit=20&active=true&private_access_token=${process.env.autoformtoken}
 `,
   );
   const personsData = await personsDataJson.json();
