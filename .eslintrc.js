b857ae8a2dafb9b70a114e8f5b2e78a048c99e20
Július Retzer <jul.retzer@gmail.com>
2020-02-29 12:05:11 +0100

    Disable

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,18 @@ module.exports = {
   parserOptions: {
     project: ["./tsconfig.json", "./cypress/tsconfig.json"],
   },
-  plugins: ["@typescript-eslint", "eslint-comments", "jest", "only-warn"],
+  plugins: [
+    "@typescript-eslint",
+    "eslint-comments",
+    "jest",
+    // "unicorn",
+    "only-warn",
+  ],
   extends: [
     "airbnb-typescript",
     "plugin:@typescript-eslint/recommended",
     "plugin:jest/recommended",
+    // "plugin:unicorn/recommended",
     "prettier",
     "prettier/react",
     "prettier/@typescript-eslint",
@@ -16,34 +23,23 @@ module.exports = {
   rules: {
     // Too restrictive, writing ugly code to defend against a very unlikely scenario: https://eslint.org/docs/rules/no-prototype-builtins
     "no-prototype-builtins": "off",
-    // https://basarat.gitbooks.io/typescript/docs/tips/defaultIsBad.html
+    // https://basarat.gitbook.io/typescript/main-1/defaultisbad
     "import/prefer-default-export": "off",
     // Too restrictive: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/destructuring-assignment.md
     "react/destructuring-assignment": "off",
-    // No jsx extension: https://github.com/facebook/create-react-app/issues/87#issuecomment-234627904
-    "react/jsx-filename-extension": "off",
+    "react/jsx-props-no-spreading": "off",
     // Use function hoisting to improve code readability
-    "no-use-before-define": [
-      "error",
-      { functions: false, classes: true, variables: false },
-    ],
+    "no-use-before-define": "off",
     // Makes no sense to allow type inferrence for expression parameters, but require typing the response
     "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-use-before-define": [
-      "error",
-      { functions: false, classes: false, variables: false, typedefs: false },
-    ],
-    // Common abbreviations are known and readable
-    "unicorn/prevent-abbreviations": "off",
-    "spaced-comment": ["error", "always", { markers: ["/"] }],
-    "jsx-a11y/anchor-is-valid": "off",
-    "react/jsx-props-no-spreading": "off",
-    "eslint-comments/disable-enable-pair": "off",
-    "import/no-default-export": 0,
-    "no-unused-expressions": "off",
+    "@typescript-eslint/no-use-before-define": "off",
     "@typescript-eslint/no-unused-expressions": [
       1,
       { allowShortCircuit: true, allowTernary: true },
     ],
+    "spaced-comment": ["error", "always", { markers: ["/"] }],
+    "eslint-comments/disable-enable-pair": "off",
+    "import/no-default-export": 0,
+    "jsx-a11y/anchor-is-valid": 0,
   },
 };
