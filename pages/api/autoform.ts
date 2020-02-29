bd0852398d46f055b763ad96f9d19b3b6ada0384
Július Retzer <jul.retzer@gmail.com>
2020-02-29 01:24:11 +0100

    Return error from api

--- pages/api/autoform.ts
+++ pages/api/autoform.ts
@@ -2,12 +2,17 @@ import fetch from "isomorphic-unfetch";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  res.statusCode = 200;
   const personsDataJson = await fetch(
     `https://autoform.ekosystem.slovensko.digital/api/corporate_bodies/search?q=name:${req.query.fullName}&limit=20&active=true&private_access_token=${process.env.autoformtoken}
 `,
   );
+
   const personsData = await personsDataJson.json();
+
+  personsData.message === "Bad credentials"
+    ? (res.statusCode = 401)
+    : (res.statusCode = 200);
+
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(personsData));
 };
