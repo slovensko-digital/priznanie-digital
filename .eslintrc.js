3f12d1b3393f3619fc983404d5974ba35df8f7b4
Július Retzer <jul.retzer@gmail.com>
2020-03-01 12:16:42 +0100

    Disable jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,58 +1,69 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: ["./tsconfig.json", "./cypress/tsconfig.json"],
-    ecmaVersion: "2020",
-    sourceType: "module",
+    project: ['./tsconfig.json', './cypress/tsconfig.json'],
+    ecmaVersion: '2020',
+    sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
     },
   },
   plugins: [
-    "@typescript-eslint",
-    "eslint-comments",
-    "jest",
-    "unicorn",
-    "only-warn",
+    '@typescript-eslint',
+    'eslint-comments',
+    // 'jest',
+    'unicorn',
+    'only-warn',
   ],
   extends: [
-    "airbnb-typescript",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:jest/recommended",
-    "plugin:unicorn/recommended",
-    "plugin:sonarjs/recommended",
-    "prettier",
-    "prettier/react",
-    "prettier/@typescript-eslint",
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    // "plugin:jest/recommended",
+    'plugin:unicorn/recommended',
+    'plugin:sonarjs/recommended',
+    'prettier',
+    'prettier/react',
+    'prettier/@typescript-eslint',
   ],
   rules: {
     // Too restrictive, writing ugly code to defend against a very unlikely scenario: https://eslint.org/docs/rules/no-prototype-builtins
-    "no-prototype-builtins": "off",
+    'no-prototype-builtins': 'off',
     // https://basarat.gitbook.io/typescript/main-1/defaultisbad
-    "import/prefer-default-export": "off",
+    'import/prefer-default-export': 'off',
     // Too restrictive: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/destructuring-assignment.md
-    "react/destructuring-assignment": "off",
-    "react/jsx-props-no-spreading": "off",
+    'react/destructuring-assignment': 'off',
+    'react/jsx-props-no-spreading': 'off',
     // Use function hoisting to improve code readability
-    "no-use-before-define": "off",
+    'no-use-before-define': 'off',
     // Makes no sense to allow type inferrence for expression parameters, but require typing the response
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-use-before-define": "off",
-    "@typescript-eslint/no-unused-expressions": [
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/no-unused-expressions': [
       1,
       { allowShortCircuit: true, allowTernary: true },
     ],
-    "spaced-comment": ["error", "always", { markers: ["/"] }],
-    "eslint-comments/disable-enable-pair": [
-      "error",
+    'spaced-comment': ['error', 'always', { markers: ['/'] }],
+    'eslint-comments/disable-enable-pair': [
+      'error',
       {
         allowWholeFile: true,
       },
     ],
-    "import/no-default-export": 0,
-    "jsx-a11y/anchor-is-valid": 0,
-    "unicorn/prevent-abbreviations": "off",
-    // "node/no-missing-import": "off",
+    'import/no-default-export': 0,
+    'jsx-a11y/anchor-is-valid': 0,
+    'unicorn/prevent-abbreviations': 'off',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          '**/*.test.js',
+          '**/*.spec.js',
+          'cypress',
+          '__tests__',
+          'setupTests.js',
+        ],
+      },
+    ],
   },
 };
