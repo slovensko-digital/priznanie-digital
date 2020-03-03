05c1e5b80b09982d0ab9a6b41bd94fc1b6c14c59
Július Retzer <jul.retzer@gmail.com>
2020-03-03 23:55:07 +0100

    Refactor calculatoin

--- src/lib/calculation.ts
+++ src/lib/calculation.ts
@@ -13,7 +13,6 @@ function parseInt10(input: string) {
 export function calculate(i: TaxFormUserInput): TaxForm {
   /** Combine default vaules with user input */
   const tf: TaxForm = {
-    datum: i.datum,
     r001_dic: i.r001_dic,
     r003_nace: i.r003_nace,
     r004_priezvisko: i.r004_priezvisko,
@@ -23,6 +22,7 @@ export function calculate(i: TaxFormUserInput): TaxForm {
     r009_psc: i.r009_psc,
     r010_obec: i.r010_obec,
     r011_stat: i.r011_stat,
+    r030: 0, // TODO in next use cases
     r031_priezvisko_a_meno: i?.r031_priezvisko_a_meno ?? '',
     r031_rodne_cislo: i?.r031_rodne_cislo ?? '',
     r032_uplatnujem_na_partnera: i?.r032_uplatnujem_na_partnera ?? false,
@@ -37,98 +37,130 @@ export function calculate(i: TaxFormUserInput): TaxForm {
       i?.r033_partner_kupele_uhrady ?? '0',
     ),
     r034: i.r034 as any, // TODO
-    t1r10_prijmy: parseInt10(i.t1r10_prijmy),
     priloha3_r11_socialne: parseInt10(i.priloha3_r11_socialne),
     priloha3_r13_zdravotne: parseInt10(i.priloha3_r13_zdravotne),
     r038: parseInt10(i?.r038 ?? '0'),
     r039: parseInt10(i?.r039 ?? '0'),
-    get r040(this: TaxForm) {
+
+    t1r10_prijmy: parseInt10(i.t1r10_prijmy),
+    get t1r2_prijmy() {
+      return this.t1r10_prijmy;
+    },
+    get t1r10_vydavky() {
+      return (
+        Math.min(this.t1r10_prijmy * 0.6, PAUSALNE_VYDAVKY_MAX) +
+        this.priloha3_r08_poistne
+      );
+    },
+    get priloha3_r08_poistne() {
+      return this.priloha3_r11_socialne + this.priloha3_r13_zdravotne;
+    },
+    get r040() {
       return this.r038 - this.r039;
     },
-  };
-  const flatrateExpenses = tf.t1r10_prijmy * 0.6;
-
-  tf.priloha3_r08_poistne =
-    tf.priloha3_r11_socialne + tf.priloha3_r13_zdravotne;
-  tf.t1r10_vydavky =
-    Math.min(flatrateExpenses, PAUSALNE_VYDAVKY_MAX) + tf.priloha3_r08_poistne;
-  tf.t1r2_prijmy = tf.t1r10_prijmy;
-
-  tf.r030 = 0; // TODO in next use cases
-
-  /** Employed */
-  tf.r040 = tf.r038 - tf.r039;
-
-  tf.r041 = tf.t1r10_prijmy;
-  tf.r042 = tf.t1r10_vydavky;
-  tf.r043 = Math.abs(tf.r041 - tf.r042);
-  tf.r047 = tf.r043; // tf.r044 + tf.r045 - tf.r046);
-  tf.r055 = tf.r047;
-  tf.r057 = tf.r055;
-  tf.r072_pred_znizenim = tf.r057 + tf.r040;
-  tf.r073 =
-    tf.r072_pred_znizenim > 20507 // TODO test both cases here
-      ? Math.max(0, 9064.094 - (1 / 4) * (tf.r072_pred_znizenim - tf.r030)) // WTF Black Magic
-      : Math.max(0, NEZDANITELNA_CAST_ZAKLADU - tf.r030);
-
-  /** Partner */
-  if (tf.r032_uplatnujem_na_partnera) {
-    tf.r074_znizenie_partner = // TODO test both cases here
-      tf.r072_pred_znizenim > 36256.38
+    get r041() {
+      return this.t1r10_prijmy;
+    },
+    get r042() {
+      return this.t1r10_vydavky;
+    },
+    get r043() {
+      return Math.abs(this.r041 - this.r042);
+    },
+    get r047() {
+      return this.r043; // this.r044 + this.r045 - this.r046);
+    },
+    get r055() {
+      return this.r047;
+    },
+    get r057() {
+      return this.r055;
+    },
+    get r072_pred_znizenim() {
+      return this.r057 + this.r040;
+    },
+    get r073() {
+      return this.r072_pred_znizenim > 20507 // TODO test both cases here
         ? Math.max(
             0,
-            (13001.438 -
-              (1 / 4) * tf.r072_pred_znizenim -
-              Math.max(tf.r032_partner_vlastne_prijmy, 0)) *
-              (1 / 12) *
-              tf.r032_partner_pocet_mesiacov,
+            9064.094 - (1 / 4) * (this.r072_pred_znizenim - this.r030),
           )
-        : Math.max(
-            0,
-            (3937.35 - Math.max(tf.r032_partner_vlastne_prijmy, 0)) *
-              (1 / 12) *
-              tf.r032_partner_pocet_mesiacov,
-          );
-  } else {
-    tf.r074_znizenie_partner = 0;
-  }
+        : Math.max(0, NEZDANITELNA_CAST_ZAKLADU - this.r030);
+    },
+    get r074_znizenie_partner() {
+      if (this.r032_uplatnujem_na_partnera) {
+        return this.r072_pred_znizenim > 36256.38
+          ? Math.max(
+              0,
+              (13001.438 -
+                (1 / 4) * this.r072_pred_znizenim -
+                Math.max(this.r032_partner_vlastne_prijmy, 0)) *
+                (1 / 12) *
+                this.r032_partner_pocet_mesiacov,
+            )
+          : Math.max(
+              0,
+              (3937.35 - Math.max(this.r032_partner_vlastne_prijmy, 0)) *
+                (1 / 12) *
+                this.r032_partner_pocet_mesiacov,
+            );
+      } else {
+        return 0;
+      }
+    },
+    get r076_kupele_spolu() {
+      return this.r076a_kupele_danovnik + this.r076b_kupele_partner_a_deti;
+    },
+    r076a_kupele_danovnik: 0, // TODO asi z inputu
+    get r076b_kupele_partner_a_deti() {
+      return this.r033_partner_kupele_uhrady; // TODO + kupele deti;
+    },
+    get r077_nezdanitelna_cast() {
+      return this.r073 + this.r074_znizenie_partner + this.r076_kupele_spolu; // TODO + tf.r075;
+    },
+    get r078_zaklad_dane_z_prijmov() {
+      return Math.max(this.r072_pred_znizenim - this.r077_nezdanitelna_cast, 0);
+    },
+    get r080_zaklad_dane_celkovo() {
+      return floor(this.r078_zaklad_dane_z_prijmov, 2); // TODO + tf.r065 + tf.r071 + tf.r079)
+    },
+    get r081() {
+      return floor(tf.r080_zaklad_dane_celkovo * DAN_Z_PRIJMU_SADZBA, 2); // TODO high income
+    },
+    get r090() {
+      return this.r081;
+    },
+    get r105_dan() {
+      return this.r081;
+    },
+    get r107() {
+      return this.r081;
+    },
+    get r113() {
+      return this.r107; // TODO - tf.r112;
+    },
+    get r125_dan_na_uhradu() {
+      return this.r105_dan;
+      // - tf.r106 +
+      // tf.r108 +
+      // tf.r110 -
+      // tf.r112 +
+      // tf.r114 +
+      // tf.r116 +
+      // tf.r117 -
+      // tf.r118 -
+      // tf.r119 -
+      // tf.r120 -
+      // tf.r121 -
+      // tf.r122 -
+      // tf.r123 -
+      // tf.r124;
+    },
+    get r126_danovy_preplatok() {
+      return Math.abs(Math.min(this.r125_dan_na_uhradu, 0));
+    },
+    datum: i.datum,
+  };
 
-  tf.r076a_kupele_danovnik = 0; // TODO asi z inputu
-  tf.r076b_kupele_partner_a_deti = tf.r033_partner_kupele_uhrady; // TODO + kupele deti;
-  tf.r076_kupele_spolu =
-    tf.r076a_kupele_danovnik + tf.r076b_kupele_partner_a_deti;
-
-  tf.r077_nezdanitelna_cast =
-    tf.r073 + tf.r074_znizenie_partner + tf.r076_kupele_spolu; // TODO + tf.r075;
-
-  tf.r078_zaklad_dane_z_prijmov = Math.max(
-    tf.r072_pred_znizenim - tf.r077_nezdanitelna_cast,
-    0,
-  );
-
-  tf.r080_zaklad_dane_celkovo = floor(tf.r078_zaklad_dane_z_prijmov, 2); // TODO + tf.r065 + tf.r071 + tf.r079)
-
-  tf.r081 = floor(tf.r080_zaklad_dane_celkovo * DAN_Z_PRIJMU_SADZBA, 2); // TODO high income
-  tf.r090 = tf.r081;
-  tf.r105_dan = tf.r081;
-  tf.r107 = tf.r081;
-  tf.r113 = tf.r107; // TODO - tf.r112;
-  tf.r125_dan_na_uhradu = tf.r105_dan;
-  // - tf.r106 +
-  // tf.r108 +
-  // tf.r110 -
-  // tf.r112 +
-  // tf.r114 +
-  // tf.r116 +
-  // tf.r117 -
-  // tf.r118 -
-  // tf.r119 -
-  // tf.r120 -
-  // tf.r121 -
-  // tf.r122 -
-  // tf.r123 -
-  // tf.r124;
-  tf.r126_danovy_preplatok = Math.abs(Math.min(tf.r125_dan_na_uhradu, 0));
-  tf.datum = i.datum;
   return tf;
 }
