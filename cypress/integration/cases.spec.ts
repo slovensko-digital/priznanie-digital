9603ae84f80b9cec3cd869c3494175a241b521ea
Július Retzer <jul.retzer@gmail.com>
2020-03-04 20:12:54 +0100

    Dynemic tests kinda working

--- cypress/integration/cases.spec.ts
+++ cypress/integration/cases.spec.ts
@@ -4,9 +4,7 @@
 /* eslint-disable promise/catch-or-return */
 /// <reference types="cypress" />
 
-import { baseInput } from '../../__tests__/testCases/baseInput';
 import { TaxFormUserInput } from '../../src/types/TaxFormUserInput';
-import { TaxForm } from '../../src/types/TaxForm';
 import { convertToXML } from '../../src/lib/xml/xmlConverter';
 import { setDate } from '../../src/lib/utils';
 import { calculate } from '../../src/lib/calculation';
@@ -15,89 +13,131 @@ function getInput<K extends keyof TaxFormUserInput>(key: K, suffix = '') {
   return cy.get(`[data-test="${key}-input${suffix}"]`);
 }
 
-const nextButton = 'Pokračovať';
+function typeToInput<K extends keyof TaxFormUserInput>(
+  key: K,
+  userInput: TaxFormUserInput,
+) {
+  const value = userInput[key];
+  if (typeof value === 'string') {
+    return getInput(key).type(value);
+  }
+  throw new Error(`Incorrect type of input: ${value}`);
+}
 
-describe('Cases', function() {
-  it('Basic', function() {
-    const now = new Date(2020, 1, 22);
-    cy.visit('/');
+function next() {
+  cy.contains('Pokračovať').click();
+}
+describe('Cases', async function() {
+  // ['base', 'withPartner', 'withEmployment'].forEach(testCase => {
+  ['base', 'withPartner', 'withEmployment'].forEach(async testCase => {
+    it(testCase, async function() {
+      const inputModule = await import(
+        `../../__tests__/testCases/${testCase}Input.ts`
+      );
 
-    cy.contains('Pripraviť daňové priznanie').click();
+      // Access named export
+      const input: TaxFormUserInput = inputModule[`${testCase}Input`];
 
-    /** Prijmy a vydavky */
-    getInput('t1r10_prijmy').type(baseInput.t1r10_prijmy.toString());
-    getInput('priloha3_r11_socialne').type(
-      baseInput.priloha3_r11_socialne.toString(),
-    );
-    getInput('priloha3_r13_zdravotne').type(
-      baseInput.priloha3_r13_zdravotne.toString(),
-    );
-    cy.contains(nextButton).click();
+      const now = new Date(2020, 1, 22);
+      cy.visit('/');
 
-    /** Zemestnanie */
-    getInput('employed', '-no').click();
+      cy.contains('Pripraviť daňové priznanie').click();
 
-    cy.contains(nextButton).click();
+      /** Prijmy a vydavky */
+      getInput('t1r10_prijmy').type(input.t1r10_prijmy.toString());
+      getInput('priloha3_r11_socialne').type(
+        input.priloha3_r11_socialne.toString(),
+      );
+      getInput('priloha3_r13_zdravotne').type(
+        input.priloha3_r13_zdravotne.toString(),
+      );
 
-    /** Partner */
-    getInput('r032_uplatnujem_na_partnera', '-no').click();
+      next();
 
-    cy.contains(nextButton).click();
+      /** Zemestnanie */
+      if (input.employed) {
+        /** Todo */
+        getInput('employed', '-yes').click();
+        typeToInput('r038', input);
+        typeToInput('r039', input);
+      } else {
+        getInput('employed', '-no').click();
+      }
 
-    /** Kids */
-    getInput('children', '-no').click();
+      next();
 
-    cy.contains(nextButton).click();
+      /** Partner */
+      if (input.r032_uplatnujem_na_partnera) {
+        getInput('r032_uplatnujem_na_partnera', '-yes').click();
+        typeToInput('r031_priezvisko_a_meno', input);
+        typeToInput('r031_rodne_cislo', input);
+        typeToInput('r032_partner_vlastne_prijmy', input);
+        typeToInput('r032_partner_pocet_mesiacov', input);
+      } else {
+        getInput('r032_uplatnujem_na_partnera', '-no').click();
+      }
 
-    /** Osobne udaje */
-    getInput('r001_dic').type(baseInput.r001_dic?.toString());
-    getInput('r003_nace').type(baseInput.r003_nace.toString());
-    getInput('r004_priezvisko').type(baseInput.r004_priezvisko.toString());
-    getInput('r005_meno').type(baseInput.r005_meno.toString());
-    getInput('r007_ulica').type(baseInput.r007_ulica.toString());
-    getInput('r008_cislo').type(baseInput.r008_cislo.toString());
-    getInput('r009_psc').type(baseInput.r009_psc.toString());
-    getInput('r010_obec').should('have.value', baseInput.r010_obec);
-    getInput('r011_stat').type(baseInput.r011_stat.toString());
+      next();
 
-    cy.contains(nextButton).click();
+      /** Kids */
+      if (input.children) {
+        getInput('children', '-yes').click();
+      } else {
+        getInput('children', '-no').click();
+      }
 
-    /** Summary */
-    cy.contains('XML');
+      next();
 
-    cy.get(`[data-test="taxFormUserInput"]`)
-      .invoke('text')
-      .then(output => {
-        const taxFormUserInput = setDate(
-          JSON.parse(output.toString()) as TaxFormUserInput,
-          now,
-        );
-        const xmlResult = convertToXML(calculate(taxFormUserInput));
+      /** Osobne udaje */
+      typeToInput('r001_dic', input);
+      typeToInput('r003_nace', input);
+      typeToInput('r004_priezvisko', input);
+      typeToInput('r005_meno', input);
+      typeToInput('r007_ulica', input);
+      typeToInput('r008_cislo', input);
+      typeToInput('r009_psc', input);
+      getInput('r010_obec').should('have.value', input.r010_obec);
+      typeToInput('r011_stat', input);
 
-        cy.visit('/form/form.451.html');
+      next();
 
-        const stub = cy.stub();
-        cy.on('window:alert', stub);
+      /** Summary */
+      cy.contains('XML');
 
-        cy.get('#form-button-load').click();
-        cy.get('#form-buttons-load-dialog > input').upload({
-          fileContent: xmlResult,
-          fileName: 'xmlResult.xml',
-          mimeType: 'application/xml',
-          encoding: 'utf-8',
-        });
+      cy.get(`[data-test="taxFormUserInput"]`)
+        .invoke('text')
+        .then(output => {
+          const taxFormUserInput = setDate(
+            JSON.parse(output.toString()) as TaxFormUserInput,
+            now,
+          );
+          const xmlResult = convertToXML(calculate(taxFormUserInput));
 
-        cy.get('#form-buttons-load-dialog-confirm > .ui-button-text').click();
-        cy.get('#form-button-validate')
-          .click()
-          .should(() => {
-            expect(stub).to.be.calledWith(
-              'Naplnenie formulára prebehlo úspešne',
-            );
+          cy.visit('/form/form.451.html');
+
+          const stub = cy.stub();
+          cy.on('window:alert', stub);
+
+          cy.get('#form-button-load').click();
+          cy.get('#form-buttons-load-dialog > input').upload({
+            fileContent: xmlResult,
+            fileName: 'xmlResult.xml',
+            mimeType: 'application/xml',
+            encoding: 'utf-8',
           });
-        cy.get('#errorsContainer').should(el => expect(el.text()).to.be.empty);
-      });
 
-    //
+          cy.get('#form-buttons-load-dialog-confirm > .ui-button-text').click();
+          cy.get('#form-button-validate')
+            .click()
+            .should(() => {
+              expect(stub).to.be.calledWith(
+                'Naplnenie formulára prebehlo úspešne',
+              );
+            });
+          cy.get('#errorsContainer').should(
+            el => expect(el.text()).to.be.empty,
+          );
+        });
+    });
   });
 });
