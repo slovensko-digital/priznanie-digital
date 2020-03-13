cf0fcd3cbda0596ddc0ef9b83dba7a65cf2e9edb
Július Retzer <jul.retzer@gmail.com>
2020-03-13 15:07:28 +0100

    WIP

---
+++ src/lib/postpone/PostponeOutput.ts
@@ -0,0 +1,51 @@
+export interface PostponeOutput {
+  dokument: Dokument;
+}
+interface Dokument {
+  hlavicka: Hlavicka;
+}
+
+interface FyzickaOsoba {
+  priezvisko: string;
+  meno: string;
+  rodneCislo: RodneCislo;
+}
+
+interface Hlavicka {
+  dic: string;
+  zaRok: string;
+  dovodDoplnenia: string;
+  fyzickaOsoba: FyzickaOsoba;
+  pravnickaOsoba: PravnickaOsoba;
+  sidlo: Sidlo;
+  adresaSr: any;
+  novaLehota: NovaLehota;
+  vypracoval: Vypracoval;
+  podpis: string;
+}
+
+interface NovaLehota {
+  predlzenie493a: string;
+  predlzenie493b: string;
+  datumLehota: string;
+}
+
+interface PravnickaOsoba {
+  obchodneMeno: any;
+}
+
+interface RodneCislo {
+  rcPredLom: string;
+  rcZaLom: string;
+}
+
+
+interface Sidlo {
+  psc: string;
+  obec: string;
+  stat: string;
+}
+
+interface Vypracoval {
+  dna: string;
+}
