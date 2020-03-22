fe1c3742a9ecc76a893c35e4cb3d65d119f3327c
Július Retzer <jul.retzer@gmail.com>
2020-03-22 14:44:29 +0100

    Feedback

--- src/components/Feedback.tsx
+++ src/components/Feedback.tsx
@@ -13,11 +13,21 @@ export const Feedback: React.FC<Props> = ({
   taxFormUserInput,
   postponeUserInput,
 }: Props) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isFeedbackOpen, setIsFeedbackOpen] = useState(false);
+  const [isUsefulSubmitted, setIsUsefulSubmitted] = useState(false);
   const [isSubmittedSuccessfully, setIsSubmittedSuccessfully] = useState(
     undefined,
   );
-  if (isOpen) {
+
+  const usefulOnClick = () => {
+    fetch(
+      'https://navody.digital/spatna-vazba?current_path=priznanie-digital-info-test&amp;feedback_type=Useful',
+      { method: 'POST' },
+    );
+    setIsUsefulSubmitted(true);
+  };
+
+  if (isFeedbackOpen) {
     return (
       <div
         id="sdn-feedbackbar-form-foundbug"
@@ -39,7 +49,7 @@ export const Feedback: React.FC<Props> = ({
               href="#"
               onClick={e => {
                 e.preventDefault();
-                setIsOpen(false);
+                setIsFeedbackOpen(false);
               }}
             >
               zatvoriť
@@ -135,14 +145,44 @@ export const Feedback: React.FC<Props> = ({
   }
   return (
     <div className="sdn-feedbackbar__container" id="sdn-feedbackbar-container">
-      <div className="govuk-grid-column-one-third">
-        <img src="/assets/images/icon-lock.svg" alt="" className="icon" />
-        <span>Bezpečné SSL pripojenie</span>
+      <div className="sdn-feedbackbar__useful">
+        {!isUsefulSubmitted ? (
+          <>
+            <span className="sdn-feedbackbar__useful-question">
+              Boli tieto informácie pre vás užitočné?
+            </span>
+            <span className="govuk-!-display-inline-block">
+              <span className="sdn-feedbackbar__yes">
+                <a
+                  className="sdn-feedbackbar__link"
+                  rel="nofollow"
+                  href="#"
+                  onClick={usefulOnClick}
+                >
+                  Áno
+                </a>
+              </span>
+              <span className="sdn-feedbackbar__no">
+                <a
+                  href="#"
+                  className="sdn-feedbackbar__link"
+                  onClick={e => {
+                    e.preventDefault();
+                    setIsFeedbackOpen(true);
+                  }}
+                >
+                  Nie
+                </a>
+              </span>
+            </span>
+          </>
+        ) : (
+          <div id="sdn-feedbackbar-thanks">
+            <span>Ďakujeme za odozvu!&nbsp;</span>
+          </div>
+        )}
       </div>
-      <div className="govuk-grid-column-one-third">
-        <img src="/assets/images/icon-correct.svg" alt="" className="icon" />
-        <span>Overené certifikovaným účtovníkom</span>
-      </div>
+
       <div className="sdn-feedbackbar__foundbug">
         <span>Našli ste na stránke chybu?&nbsp;</span>
         <span>
@@ -152,7 +192,7 @@ export const Feedback: React.FC<Props> = ({
             data-test="feedback"
             onClick={e => {
               e.preventDefault();
-              setIsOpen(true);
+              setIsFeedbackOpen(true);
             }}
           >
             Napíšte nám
