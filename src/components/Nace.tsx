8a3c92fe5eab72e9dd9aca67cb4e46708979525e
Július Retzer <jul.retzer@gmail.com>
2020-03-24 16:25:32 +0100

    Styled nace

--- src/components/Nace.tsx
+++ src/components/Nace.tsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import Autosuggest from 'react-autosuggest';
+import Autosuggest, { Theme } from 'react-autosuggest';
 import { useField } from 'formik';
+import classnames from 'classnames';
 import { nace } from '../lib/api';
 
 interface Props {
-  className: string;
+  label: string;
+  hint?: string;
+  className?: string;
+  width?: 30 | 20 | 10 | 5 | 4 | 3 | 2 | 'auto';
 }
 
 interface NaceItem {
@@ -15,9 +19,26 @@ interface NaceItem {
   score: number;
 }
 
-export const Nace: React.FC<Props> = ({ className }: Props) => {
+interface SuggestionProps {
+  suggestion: NaceItem;
+}
+const Suggestion: React.FC<SuggestionProps> = ({
+  suggestion,
+}: SuggestionProps) => (
+  <div>
+    {suggestion?.item?.code} - {suggestion?.item?.label}
+  </div>
+);
+
+export const Nace: React.FC<Props> = ({
+  className = '',
+  label = 'NACE',
+  width = 'auto',
+  hint = '',
+}: Props) => {
+  const name = 'r003_nace';
   const [naceItems, setNaceItems] = useState<NaceItem[]>([]);
-  const [field] = useField('r003_nace');
+  const [field, meta] = useField(name);
 
   const fetchData = async ({ value }) => {
     const result = await nace(value);
@@ -33,18 +54,54 @@ export const Nace: React.FC<Props> = ({ className }: Props) => {
 
   const inputProps = {
     ...field,
-    name: 'r003_nace',
+    name,
+    id: name,
+    className: classnames('govuk-input', {
+      [`govuk-input--width-${width}`]: width !== 'auto',
+    }),
+    'data-test': `${field.name}-input`,
   };
-  
+
+  const theme: Theme = {
+    suggestionsContainerOpen:
+      'govuk-list govuk-list--number autocomplete__menu',
+    suggestion: 'autocomplete__option',
+    suggestionHighlighted: 'autocomplete__option--focused',
+    suggestionsList: { padding: 0, margin: 0 },
+  };
+
   return (
-    <div className={className}>
+    <div
+      className={classnames([
+        'govuk-form-group',
+        className,
+        meta.touched && meta.error && 'govuk-form-group--error',
+      ])}
+    >
+      <label className="govuk-label govuk-!-font-weight-bold" htmlFor={name}>
+        {label}
+      </label>
+
+      {meta.touched && meta.error ? (
+        <span
+          id={name}
+          data-test="error"
+          className="govuk-error-message govuk-!-margin-top-2 govuk-!-margin-bottom-0"
+        >
+          <span className="govuk-visually-hidden">Error:</span> {meta.error}
+        </span>
+      ) : null}
+
+      <span className="govuk-hint">{hint}</span>
+
       <Autosuggest<NaceItem>
         onSuggestionsFetchRequested={fetchData}
         onSuggestionsClearRequested={onSuggestionsClearRequested}
         getSuggestionValue={suggestion => suggestion?.item?.label}
         suggestions={naceItems}
-        renderSuggestion={suggestion => <div>{suggestion?.item?.label}</div>}
+        renderSuggestion={suggestion => <Suggestion suggestion={suggestion} />}
         inputProps={inputProps}
+        theme={theme}
       />
     </div>
   );
