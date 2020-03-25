ffcb354685b2619def91b6faea647fcfbc499d3a
Július Retzer <jul.retzer@gmail.com>
2020-03-25 21:40:00 +0100

    non generic

--- src/types/TaxFormUserInput.ts
+++ src/types/TaxFormUserInput.ts
@@ -17,7 +17,7 @@ export interface Child {
   m12: boolean
 }
 
-export interface TaxFormUserInputBase<T> {
+export interface TaxFormUserInput {
   /**   01 - DIČ (ak nie je pridelené| uvádza sa rodné číslo)*/
   r001_dic: string
   /** 03 - SK NACE - Hlavná, prevažná činnosť*/
@@ -39,38 +39,38 @@ export interface TaxFormUserInputBase<T> {
   /** Poberal (a) som na začiatku zdaňovacieho obdobia dôchodok (ky) uvedený (é) v § 11 ods. 6 zákona alebo mi bol tento (tieto) dôchodok (ky) priznaný (é) spätne k začiatku príslušného zdaňovacieho obdobia (vypĺňa sa, len ak daňovník bol poberateľom dôchodku uvedeného v § 11 ods. 6 zákona na začiatku zdaňovacieho obdobia alebo mu tento dôchodok bol priznaný spätne k začiatku príslušného zdaňovacieho obdobia)  */
   r029_poberal_dochodok?: boolean
   /** Úhrnná suma dôchodku (ov) uvedeného (ných) v § 11 ods. 6 zákona za zdaňovacie obdobie (v eurách) */
-  r030_vyska_dochodku?: T
+  r030_vyska_dochodku?: string
 
   /** SECTION  Partner*/
   r031_priezvisko_a_meno?: string
   r031_rodne_cislo?: string
   r032_uplatnujem_na_partnera?: boolean
-  r032_partner_vlastne_prijmy?: T
-  r032_partner_pocet_mesiacov?: T
+  r032_partner_vlastne_prijmy?: string
+  r032_partner_pocet_mesiacov?: string
   r033_partner_kupele?: boolean
   /**   max 50*/
-  r033_partner_kupele_uhrady?: T
+  r033_partner_kupele_uhrady?: string
 
   /** SECTION  Mortage */
   r037_uplatnuje_uroky?: boolean
-  r037_zaplatene_uroky?: T
-  r037_pocetMesiacov?: T
+  r037_zaplatene_uroky?: string
+  r037_pocetMesiacov?: string
 
   /** SECTION Prijmy a poistenie  */
   /**   VIs.Príjmy z tabuľky č. 1, stĺ. 1, r. 10 TODO asi zrkadlenie do VI.Príjmy z tabuľky č. 1, stĺ. 1, r. 2*/
-  t1r10_prijmy: T
+  t1r10_prijmy: string
   /**   Preukázateľne zaplatené poistné na sociálne poistenie z príjmov*/
-  priloha3_r11_socialne: T
+  priloha3_r11_socialne: string
   /**   Preukázateľne zaplatené poistné na zdravotné poistenie z príjmov*/
-  priloha3_r13_zdravotne: T
+  priloha3_r13_zdravotne: string
   /**   Zamestnanie */
-  r038?: T
-  r039?: T
+  r038?: string
+  r039?: string
 
   /**   Deti*/
   r034?: Child[]
   /** Preukázateľne zaplatené úhrady na kúpelnú starostlivosť za vyživované dieťa (deti) v úhrne najviac do výšky 50 eur za rok za každé z týchto detí  */
-  r036?: T
+  r036?: string
 
   children?: boolean
   employed?: boolean
@@ -78,5 +78,3 @@ export interface TaxFormUserInputBase<T> {
    * failovali test */
   datum: string
 }
-
-export type TaxFormUserInput<T = string> = TaxFormUserInputBase<T>
