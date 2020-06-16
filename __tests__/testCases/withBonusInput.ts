f21bfe470b1038da28cfcd972c28d54d72e5971a
Július Retzer <jul.retzer@gmail.com>
2020-06-16 16:54:39 +0200

    Add bonus use case

---
+++ __tests__/testCases/withBonusInput.ts
@@ -0,0 +1,55 @@
+import { TaxFormUserInput } from '../../src/types/TaxFormUserInput'
+
+export const withBonusInput: TaxFormUserInput = {
+  priloha3_r11_socialne: '1000',
+  priloha3_r13_zdravotne: '1000',
+  meno_priezvisko: 'Fake Name',
+  r001_dic: '233123123',
+  r003_nace: '62010 - Počítačové programovanie',
+  r007_ulica: 'Mierova',
+  r008_cislo: '4',
+  r009_psc: '82105',
+  r010_obec: 'Bratislava 3',
+  r011_stat: 'Slovensko',
+  t1r10_prijmy: '25000',
+  datum: '22.02.2020',
+
+  /** SECTION Partner */
+  r031_priezvisko_a_meno: 'Fake Fake',
+  r031_rodne_cislo: '9609226286',
+  r032_partner_pocet_mesiacov: '12',
+  r032_partner_vlastne_prijmy: '3000',
+  r032_uplatnujem_na_partnera: true,
+
+  /** SECTION Kids */
+  children: [
+    {
+      id: 1,
+      priezviskoMeno: 'Morty Smith',
+      rodneCislo: '1607201167',
+      kupelnaStarostlivost: true,
+      wholeYear: true,
+      monthFrom: '6',
+      monthTo: '11',
+    },
+    {
+      id: 2,
+      priezviskoMeno: 'Summer Smith',
+      rodneCislo: '1057201167',
+      kupelnaStarostlivost: true,
+      wholeYear: true,
+      monthFrom: '6',
+      monthTo: '11',
+    },
+  ],
+  hasChildren: true,
+
+  /** SECTION SPA */
+  kupele: true,
+  r033_partner_kupele: true,
+  r033_partner_kupele_uhrady: '45',
+  childrenInSpa: true,
+  r036_deti_kupele: '60',
+  danovnikInSpa: true,
+  r076a_kupele_danovnik: '41',
+}
