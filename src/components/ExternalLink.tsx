48573fc5e45a704f7a7789734f55bc7319e1c9a2
Matej Lednicky <matej.lednicky@typeform.com>
2020-06-16 23:48:32 +0200

    Open footer links in new window to prevent clearing the form

---
+++ src/components/ExternalLink.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export interface ExternalLinkProps
+  extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+  href: string
+  children: React.ReactNode
+}
+
+export const ExternalLink = (props: ExternalLinkProps) => {
+  const { href, children, ...anchorProps } = props
+  return (
+    <a href={href} target="_blank" rel="noopener noreferrer" {...anchorProps}>
+      {children}
+    </a>
+  )
+}
