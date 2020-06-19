17681084a656b122ca474c9dab74fd31cb463c36
Matej Lednicky <matej.lednicky@typeform.com>
2020-06-19 14:47:21 +0200

    Add logo image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,7 +11,11 @@ const Header: React.FC = () => {
       <div className="sdn-header__container govuk-width-container ">
         <Link href="/">
           <a className="sdn-header__link">
-            <span className="sdn-header__product-name">priznanie.digital</span>
+            <img
+              className="sdn-header__logo"
+              src="/assets/images/priznanie_logo.png"
+              alt="priznanie.digital"
+            />
           </a>
         </Link>
         {/* TODO use later */}
