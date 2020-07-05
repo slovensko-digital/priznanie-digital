8cc15c89d5a73d63b9cbf7555bf99b11e32298cb
Július Retzer <jul.retzer@gmail.com>
2020-07-05 22:27:39 +0200

    Renable tests

--- cypress/integration/cases3.spec.ts
+++ cypress/integration/cases3.spec.ts
@@ -2,10 +2,10 @@ import { exectueTestcase } from './executeCase'
 
 describe('Cases', () => {
   ;[
-    // 'withEmploymentBonus',
-    // 'withHighIncome',
-    // 'bugReport1',
-    // 'bugReport2',
+    'withEmploymentBonus',
+    'withHighIncome',
+    'bugReport1',
+    'bugReport2',
     'bugReport3',
   ].forEach(exectueTestcase)
 })
