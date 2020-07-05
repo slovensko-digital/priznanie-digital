7f71f382dbb06d276d3472f39ca39941b0c15484
Július Retzer <jul.retzer@gmail.com>
2020-07-05 17:46:55 +0200

    Add meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,6 +14,26 @@ class MyDocument extends Document {
         <Head>
           <GoogleAnalytics id="UA-69285708-14" />
           <link rel="icon" href="/favicon.ico" />
+
+          <!-- Primary Meta Tags -->
+          <title>Elektronické daňové priznanie</title>
+          <meta name="title" content="Elektronické daňové priznanie"/>
+          <meta name="description" content="Daňové priznanie pre živnostníkov s paušálnymi výdavkami (DPFO typ B)"/>
+
+          <!-- Open Graph / Facebook -->
+          <meta property="og:type" content="website"/>
+          <meta property="og:url" content="https://priznanie.digital/"/>
+          <meta property="og:title" content="Elektronické daňové priznanie"/>
+          <meta property="og:description" content="Daňové priznanie pre živnostníkov s paušálnymi výdavkami (DPFO typ B)"/>
+          <meta property="og:image" content="/assets/meta-img.png"/>
+
+          <!-- Twitter -->
+          <meta property="twitter:card" content="summary_large_image"/>
+          <meta property="twitter:url" content="https://priznanie.digital/"/>
+          <meta property="twitter:title" content="Elektronické daňové priznanie"/>
+          <meta property="twitter:description" content="Daňové priznanie pre živnostníkov s paušálnymi výdavkami (DPFO typ B)"/>
+          <meta property="twitter:image" content="/assets/meta-img.png"/>
+
         </Head>
         <body>
           <Main />
