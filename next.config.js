6e51d2d4b67ffd50ec3adf29015b73dcb04e67ad
Július Retzer <jul.retzer@gmail.com>
2020-07-07 18:13:30 +0200

    Remove dot env

--- next.config.js
+++ next.config.js
@@ -1,26 +1,13 @@
-require('dotenv').config();
-
-const path = require('path');
-const Dotenv = require('dotenv-webpack');
-
 module.exports = {
   typescript: {
     ignoreDevErrors: true,
   },
-  webpack: webpackConfig => {
-    const config = {...webpackConfig};
-    config.plugins = config.plugins || [];
+  webpack: (webpackConfig) => {
+    const config = { ...webpackConfig }
+    config.plugins = config.plugins || []
 
-    config.plugins = [
-      ...config.plugins,
+    config.plugins = [...config.plugins]
 
-      // Read the .env file
-      new Dotenv({
-        path: path.join(__dirname, '.env'),
-        systemvars: true,
-      }),
-    ];
-
-    return config;
+    return config
   },
-};
+}
