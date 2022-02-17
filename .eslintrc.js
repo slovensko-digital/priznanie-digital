21c87d3162ce3f0926c60fc68ae5d336af284826
Július Retzer <jul.retzer@gmail.com>
2022-02-17 22:53:04 +0100

    Simple lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,13 @@ module.exports = {
   env: {
     jest: true,
   },
-  plugins: ['eslint-comments', 'jest', 'unicorn', 'promise', 'only-warn'],
+  plugins: [
+    // 'eslint-comments',
+    //  'jest',
+    //   'unicorn',
+    //   'promise',
+    //   'only-warn'
+  ],
   extends: [
     'react-app',
     // 'plugin:promise/recommended',
@@ -16,28 +22,28 @@ module.exports = {
   ],
   rules: {
     'jsx-a11y/anchor-is-valid': 0,
-    // we use `null` in many places right now nad do not know implications of replacing it with `undefined`
-    // reasoning for `undefined`: https://github.com/eslint/eslint/issues/6701
-    'unicorn/no-null': 0,
-    'unicorn/prevent-abbreviations': 'off',
-    'unicorn/filename-case': [
-      'error',
-      {
-        cases: {
-          camelCase: true,
-          pascalCase: true,
-          kebabCase: true,
-        },
-      },
-    ],
-    'jsx-a11y/label-has-associated-control': [
-      2,
-      {
-        assert: 'htmlFor',
-      },
-    ],
-    'sonarjs/no-duplicate-string': 0,
-    'sonarjs/cognitive-complexity': 0,
-    'react/react-in-jsx-scope': 0,
+    // // we use `null` in many places right now nad do not know implications of replacing it with `undefined`
+    // // reasoning for `undefined`: https://github.com/eslint/eslint/issues/6701
+    // 'unicorn/no-null': 0,
+    // 'unicorn/prevent-abbreviations': 'off',
+    // 'unicorn/filename-case': [
+    //   'error',
+    //   {
+    //     cases: {
+    //       camelCase: true,
+    //       pascalCase: true,
+    //       kebabCase: true,
+    //     },
+    //   },
+    // ],
+    // 'jsx-a11y/label-has-associated-control': [
+    //   2,
+    //   {
+    //     assert: 'htmlFor',
+    //   },
+    // ],
+    // 'sonarjs/no-duplicate-string': 0,
+    // 'sonarjs/cognitive-complexity': 0,
+    // 'react/react-in-jsx-scope': 0,
   },
 }
