ab0f5d554696c3db1df79bde8b3f5d28e26abd84
Matej Lednicky <matej@lednicky.name>
2022-12-14 16:31:35 +0100

    fix: Call autoform endpoint from client (#678)
    The token is public and hardcoded in next.config.js
    
    Co-authored-by: Marek Vigas <vigas147@gmail.com>

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ module.exports = {
     plausibleDomain: process.env.NEXT_PUBLIC_plausibleDomain,
     odkladEmailTemplateId: process.env.NEXT_PUBLIC_odkladEmailTemplateId,
     priznanieEmailTemplateId: process.env.NEXT_PUBLIC_priznanieEmailTemplateId,
+    autoformPublicToken: '61e4225378747a32f0e65ddd106a6fc18f5f82e81d58a539d86178a09128e47342ddc5d47ffe4073'
   },
   typescript: {
     ignoreDevErrors: true,
