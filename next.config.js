e6fcd9c7c9a06bb87b48dbd1d2b3628261e907a3
Marek Vigas <vigas147@gmail.com>
2023-03-10 08:50:13 +0100

    fix typo

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
   publicRuntimeConfig: {
     navodyBaseUrl: process.env.NEXT_PUBLIC_navodyBaseUrl,
     priznanieStepUrl: process.env.NEXT_PUBLIC_priznanieStepUrl,
-    odkladStepUrl: process.env.process.env.NEXT_PUBLIC_odkladStepUrl,
+    odkladStepUrl: process.env.NEXT_PUBLIC_odkladStepUrl,
     plausibleDomain: process.env.NEXT_PUBLIC_plausibleDomain,
     odkladEmailTemplateId: process.env.NEXT_PUBLIC_odkladEmailTemplateId,
     priznanieEmailTemplateId: process.env.NEXT_PUBLIC_priznanieEmailTemplateId,
