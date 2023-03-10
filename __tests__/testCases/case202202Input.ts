9b6859f4d8ba59d1e2b5535cd9034c6f9d6eb737
Marek Vigaš <vigas147@gmail.com>
2023-03-10 11:38:54 +0100

    2022 test cases in cypress (#722)
    * add test cases from avenias for 2022
    
    * change types to string
    
    * allow to fill before july dates

---
+++ __tests__/testCases/case202202Input.ts
@@ -0,0 +1,74 @@
+import { E2eTestUserInput } from '../../src/types/E2eTestUserInput'
+
+export const case202202Input: E2eTestUserInput = {
+  t1r10_prijmy: '78000',
+  priloha3_r11_socialne: '2253.36',
+  priloha3_r13_zdravotne: '951.72',
+  zaplatenePreddavky: '0',
+  r001_dic: '233123123',
+  r003_nace: '62010 - Počítačové programovanie',
+  r005_meno: 'Fake',
+  r004_priezvisko: 'Name',
+  r006_titul: 'Ing.',
+  r006_titul_za: 'PhD.',
+  r007_ulica: 'Mierova',
+  r008_cislo: '4',
+  r009_psc: '82105',
+  r010_obec: 'Bratislava 3',
+  r011_stat: 'Slovensko',
+  datum: '22.02.2022',
+
+  /** SECTION Employment */
+  uhrnPrijmovOdVsetkychZamestnavatelov: '0',
+  uhrnPovinnehoPoistnehoNaSocialnePoistenie: '0',
+  uhrnPovinnehoPoistnehoNaZdravotnePoistenie: '0',
+  uhrnPreddavkovNaDan: '0',
+  udajeODanovomBonuseNaDieta: '0',
+  employed: false,
+
+  /** SECTION Mortgage */
+  r037_uplatnuje_uroky: true,
+  r037_zaplatene_uroky: '200',
+  r037_pocetMesiacov: '12',
+
+  /** SECTION Pension */
+  platil_prispevky_na_dochodok: false,
+  zaplatene_prispevky_na_dochodok: '0',
+
+  /** SECTION Partner */
+  r031_priezvisko_a_meno: 'Fake Fake',
+  r031_rodne_cislo: '9609226286',
+  r032_partner_pocet_mesiacov: '12',
+  r032_partner_vlastne_prijmy: '0',
+  r032_uplatnujem_na_partnera: true,
+  partner_spolocna_domacnost: true,
+  partner_bonus_uplatneny: false,
+  partner_podmienky: { '1': true },
+
+  /** SECTION Children */
+  children: [
+    {
+      id: 1,
+      priezviskoMeno: 'Morty Smith',
+      rodneCislo: '0606070014',
+      wholeYear: true,
+      monthFrom: '6',
+      monthTo: '11',
+    },
+    {
+      id: 2,
+      priezviskoMeno: 'Summer Smith',
+      rodneCislo: '220908/0016',
+      wholeYear: false,
+      monthFrom: `8`,
+      monthTo: '11',
+    },
+  ],
+  hasChildren: true,
+  prijmyPredJul22: true,
+  zaciatokPrijmovDen: null,
+  zaciatokPrijmovMesiac: null,
+  zaciatokPrijmovRok: null,
+
+  expectNgoDonationPage: true,
+}
---
+++ cypress/e2e/cases4.spec.ts
@@ -0,0 +1,11 @@
+import { executeAllTestCases } from './executeCase'
+
+describe('Cases', () => {
+  executeAllTestCases([
+    'case202201',
+    'case202202',
+    'case202203',
+    'case202204',
+    'case202205',
+  ])
+})
