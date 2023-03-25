a9c1fc57fbfc35d9d47e5be1a2497ab1a7a30a74
janmurin2 <128835937+janmurin2@users.noreply.github.com>
2023-03-25 16:16:58 +0100

    filter legal forms (#765)

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -6,6 +6,19 @@ const {
   publicRuntimeConfig: { autoformPublicToken },
 } = getConfig()
 
+const legal_form_out = ['Iná organizácia verejnej správy', 'Iná právnicka osoba', 'Jednoduchá spoločnosť na akcie',
+  'Komanditná spoločnosť', 'Komora (s výnimkou profesných komôr)', 'Medzinárodné organizácie a združenia', 'Nadácia',
+  'Národná banka Slovenska', 'Národný výbor', 'Neinvestičný fond', 'Nešpecifikovaná právna forma', 'Nezisková organizácia',
+  'Nezisková organizácia poskytujúca všeobecne prospešné služby', 'Obecný podnik', 'Obecný úrad', 'Obec (obecný úrad), mesto (mestský úrad)',
+  'Organizačná zložka podniku', 'Politická strana, politické hnutie', 'Poľovnícka organizácia', 'Pozemkové spoločenstvo',
+  'Príspevková organizácia', 'Rozpočtová organizácia', 'Rozpočtové a príspevkové organizácie', 'Samosprávny kraj (úrad samosprávneho kraja)',
+  'Sociálna a zdravotné poisťovne', 'Spoločenstvá vlastníkov pozemkov, bytov a pod', 'Spoločenstvo vlastníkov bytov a nebytových priestorov',
+  'Spoločnosť s ručením obmedzeným', 'Štátny podnik', 'Stavovská organizácia - profesná komora', 'Verejná obchodná spoločnosť',
+  'Verejná výskumná inštitúcia', 'Verejnoprávna inštitúcia', 'Vysoká škola', 'Zahraničná osoba, právnická osoba so sídlom mimo územia SR',
+  'Zahraničné kultúrne, informačné stredisko, rozhlasová, tlačová a televízna agentúra', 'Zastúpenie zahraničnej právnickej osoby',
+  'Zastupiteľské orgány iných štátov', 'Záujmové združenie právnických osôb', 'Združenie účastníkov pozemkových úprav',
+  'Združenie (zväz, spolok, spoločnosť, klub ai.)'];
+
 export const getAutoformByPersonName = async (
   name: string,
 ): Promise<AutoFormSubject[]> => {
@@ -20,9 +33,12 @@ export const getAutoformByPersonName = async (
       `${baseUrl}/search?q=${query}&limit=${limit}&access_token=${autoformPublicToken}&filter=active`,
     )
 
-    return await response.json()
+    const data = await response.json()
+    return data.filter(subject => !legal_form_out.includes(subject.legal_form));
+
   } catch (error) {
     console.error(error)
+
     return []
   }
 }
