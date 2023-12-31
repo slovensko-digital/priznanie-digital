8802e210c449736fd7480e65e2666ec280c0f315
Marek Vigas <vigas147@gmail.com>
2023-12-31 12:25:21 +0100

    redirect on debug enable/disable

--- src/pages/debug.tsx
+++ src/pages/debug.tsx
@@ -6,14 +6,20 @@ export default function DebugPage() {
       <h1 className="govuk-heading-l govuk-!-margin-top-3">Debug stránka</h1>
       <button
         className="govuk-button"
-        onClick={() => (document.cookie = 'you-shall=not-pass')}
+        onClick={() => {
+          document.cookie = 'you-shall=not-pass'
+          window.location.href = '/'
+        }}
       >
         Zapnúť DEBUG mode
       </button>
       <br />
       <button
         className="govuk-button btn-secondary"
-        onClick={() => (document.cookie = 'you-shall=; Max-Age=0')}
+        onClick={() => {
+          document.cookie = 'you-shall=; Max-Age=0'
+          window.location.href = '/'
+        }}
       >
         Vypnúť DEBUG mode
       </button>
