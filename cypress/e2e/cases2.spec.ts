4b045274bd154f435e693ee1f5c74ab2c658243b
Marek Vigas <vigas147@gmail.com>
2024-02-08 01:04:23 +0100

    fix riadok 96

--- cypress/e2e/cases2.spec.ts
+++ cypress/e2e/cases2.spec.ts
@@ -2,13 +2,13 @@ import { executeAllTestCases } from './executeCase'
 
 describe('Cases', () => {
   executeAllTestCases([
-    // 'with2percent',
-    // 'with3percent',
-    // 'withBonus',
-    // 'withTaxReturn',
-    // 'withEmploymentBonus',
-    // 'withHighIncome',
+    'with2percent',
+    'with3percent',
+    'withBonus',
+    'withTaxReturn',
+    'withEmploymentBonus',
+    'withHighIncome',
     'withHighIncome2',
-    // 'withHighIncome3',
+    'withHighIncome3',
   ])
 })
