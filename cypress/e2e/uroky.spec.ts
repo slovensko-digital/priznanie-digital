1256f0fc76c58c90377d67aabd720be22fddfec4
Marek Vigas <vigas147@gmail.com>
2024-02-22 23:58:59 +0100

    ad first tests for uroky

---
+++ cypress/e2e/uroky.spec.ts
@@ -0,0 +1,11 @@
+import { executeAllTestCases } from './executeCase'
+
+describe('Uroky', () => {
+  executeAllTestCases([
+    'uroky202301',
+    // 'uroky202302',
+    // 'uroky202303',
+    // 'uroky202304',
+    // 'uroky202305',
+  ])
+})
