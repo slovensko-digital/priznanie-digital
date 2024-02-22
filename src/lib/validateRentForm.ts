0f8ead4bce9ffbb0e33c8629de29845bdc446c05
Ormi <mi.ormos@gmail.com>
2024-02-22 13:48:54 +0100

    [+] make validations work

--- src/lib/validateRentForm.ts
+++ src/lib/validateRentForm.ts
@@ -1,4 +1,6 @@
 import { RentUserInput } from '../types/PageUserInputs'
+import { parseInputNumber } from './utils'
+
 //import { parseInputNumber } from './utils'
 
 export const validateRentForm = (
@@ -7,15 +9,15 @@ export const validateRentForm = (
 ): boolean => {
   const step1 = true // first step is not validated
 
-  const step2 = true
+  const step2 = parseInputNumber(values.vyskaPrijmovZPrenajmu) > 0
 
-  const step3 = true
+  const step3 = values.prijemZPrenajmuOslobodenieDane === true
 
-  const step4 = true
+  const step4 = parseInputNumber(values.vyskaOslobodenia) < 500
 
-  const step5 = true
+  const step5 = parseInputNumber(values.vydavkyZPrenajmu) >= 0
 
-  const step6 = true
+  const step6 = values.rent_uctovnictvo_danova_evidencia === true || values.rent_uctovnictvo_jednoduche === true || values.rent_uctovnictvo_podvojne === true
 
   const steps = [step1, step2, step3, step4, step5, step6]
 
