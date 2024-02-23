3ea649dd6f16c529d94d8f350310b598a6b70ac8
Marek Vigas <vigas147@gmail.com>
2024-02-23 11:41:37 +0100

    add more test cases

---
+++ __tests__/testCases/uroky202303cInput.ts
@@ -0,0 +1,15 @@
+import { TAX_YEAR } from '../../src/lib/calculation'
+import { E2eTestUserInput } from '../../src/types/E2eTestUserInput'
+import { case202303Input } from './case202303Input'
+
+export const uroky202303cInput: E2eTestUserInput = {
+  ...case202303Input,
+  r035_uplatnuje_uroky: true,
+  uroky_rok_uzatvorenia: TAX_YEAR.toString(),
+  uroky_zaciatok_urocenia_den: '21',
+  uroky_zaciatok_urocenia_mesiac: '8',
+  uroky_zaciatok_urocenia_rok: TAX_YEAR.toString(),
+  uroky_dalsi_dlznik: true,
+  uroky_pocet_dlznikov: '2',
+  r035_zaplatene_uroky: '987.65',
+}
