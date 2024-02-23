3c59bf2a20a482354c91f23dd1be703f5ae0c812
Marek Vigas <vigas147@gmail.com>
2024-02-23 10:36:26 +0100

    add new test cases

---
+++ __tests__/testCases/uroky202302aInput.ts
@@ -0,0 +1,15 @@
+import { TAX_YEAR, UROKY_POCET_ROKOV } from '../../src/lib/calculation'
+import { E2eTestUserInput } from '../../src/types/E2eTestUserInput'
+import { case202302Input } from './case202302Input'
+
+export const uroky202302aInput: E2eTestUserInput = {
+  ...case202302Input,
+  r035_uplatnuje_uroky: true,
+  uroky_rok_uzatvorenia: (TAX_YEAR - UROKY_POCET_ROKOV).toString(),
+  uroky_zaciatok_urocenia_den: '21',
+  uroky_zaciatok_urocenia_mesiac: '8',
+  uroky_zaciatok_urocenia_rok: (TAX_YEAR - UROKY_POCET_ROKOV).toString(),
+  uroky_dalsi_dlznik: true,
+  uroky_pocet_dlznikov: '2',
+  r035_zaplatene_uroky: '987.65',
+}
--- cypress/e2e/uroky.spec.ts
+++ cypress/e2e/uroky.spec.ts
@@ -3,9 +3,14 @@ import { executeAllTestCases } from './executeCase'
 describe('Uroky', () => {
   executeAllTestCases([
     'uroky202301',
+    'uroky202301b',
     'uroky202302',
+    'uroky202302b',
     'uroky202303',
+    'uroky202303b',
     'uroky202304',
+    'uroky202304b',
     'uroky202305',
+    'uroky202305b',
   ])
 })
