3f5bde1b0f357ae8a3fd5c93adf5377b2aa784e9
Marek Vigas <vigas147@gmail.com>
2024-03-16 10:24:26 +0100

    run 200 tests

--- cypress/e2e/random.spec.ts
+++ cypress/e2e/random.spec.ts
@@ -170,7 +170,7 @@ describe('Random inputs', () => {
   it('should pass', () => {
     expect(Number(randomFromRange(1, 10))).to.be.within(1, 10)
   })
-  Array.from({ length: 50 }).forEach((_, i) => {
+  Array.from({ length: 200 }).forEach((_, i) => {
     it(`should pass random input ${i}`, (done) => {
       const input = randomInput()
       cy.writeFile(`cypress/downloads/randomInput${i}.json`, input)
