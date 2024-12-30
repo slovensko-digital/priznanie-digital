890649b69f8a816b032c5b006b197bdc835c1add
Marek Vigas <vigas147@gmail.com>
2024-12-30 22:04:56 +0100

    enable postpone form in dec of tax year

--- cypress/e2e/postponeCases.spec.ts
+++ cypress/e2e/postponeCases.spec.ts
@@ -1,15 +1,16 @@
+import { TAX_YEAR } from '../../src/lib/calculation'
 import { executeAllPostponeCases } from './executeCase'
 
 // formular na odklad funguje len v ramci obdobia ked sa da podat riadne danove priznanie
 // inak formular hadze chyby
 // v podstate nas zaujima len mesiac, nie rok ani den
-const date = new Date()
-const month = date.getMonth() + 1
+const now = new Date()
+const month = now.getMonth() + 1
 
 // testy na podavanie odkladu padaju ked sa pustia po lehote na podanie odkladu kvoli datumu
 // datum sa da namockovat v setDate() (src/lib/utils.js)
 // test bezi len ak je formular o odklad aktualny od Januara do konca Marca
-if (month >= 1 && month < 4) {
+if (month >= 1 && month < 4 || now.getFullYear() == TAX_YEAR) {
   describe('Postpone cases', () => {
     executeAllPostponeCases(['basic', 'foreignIncome'])
   })
