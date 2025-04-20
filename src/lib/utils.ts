545c2ae4b0aaa0053b81f3cb4ddef6de63c35e82
Katarína Zápražná <katarina.zaprazna@bratislava.sk>
2025-04-20 22:56:27 +0200

    Use non-breaking space in formatCurrency function & run prettier

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,11 +6,9 @@ import { MAX_CHILD_AGE_BONUS, monthToKeyValue, TAX_YEAR } from './calculation'
 
 export const sortObjectKeys = (object: object) => {
   const ordered = {}
-  Object.keys(object)
-    .sort()
-    .forEach((key) => {
-      ordered[key] = object[key]
-    })
+  for (const key of Object.keys(object).sort()) {
+    ordered[key] = object[key]
+  }
   return ordered
 }
 
@@ -25,11 +23,7 @@ export const formatDate = (date: Date): string => {
 export const setDate = <T>(input: T, date: Date = null) => {
   if (date === null) {
     const now = new Date()
-    if (now.getFullYear() === TAX_YEAR) {
-      date = new Date(TAX_YEAR + 1, 0, 1)
-    } else {
-      date = now
-    }
+    date = now.getFullYear() === TAX_YEAR ? new Date(TAX_YEAR + 1, 0, 1) : now
   }
   return {
     ...input,
@@ -60,7 +54,7 @@ export const formatCurrency = (value: number): string => {
   const findPlaceForThousandsDivider = /\B(?=(\d{3})+(?!\d))/g
   const roundNumber = decimalToString(new Decimal(value || 0))
   const formattedNumber = roundNumber
-    .replace(findPlaceForThousandsDivider, ' ')
+    .replace(findPlaceForThousandsDivider, '\u00A0')
     .replace('.', ',')
   return `${formattedNumber} EUR`
 }
@@ -84,11 +78,9 @@ export const translit = (value: string) => {
 
 export const formatRodneCislo = (newValue: string, previousValue = '') => {
   const formattedNewValue = newValue.replace(/\D/g, '')
-  if (`${newValue} ` === previousValue) {
-    return newValue.slice(0, -3)
-  } else {
-    return formattedNewValue.replace(/^(\d{6})/, '$1 / ')
-  }
+  return `${newValue} ` === previousValue
+    ? newValue.slice(0, -3)
+    : formattedNewValue.replace(/^(\d{6})/, '$1 / ')
 }
 
 export const validateRodneCislo = (value: string): boolean => {
@@ -161,11 +153,9 @@ export const formatIban = (newValue: string, previousValue = '') => {
   const prefix = newValue.trim().slice(0, 2)
   const number = newValue.trim().slice(2).replace(/\D/g, '')
   const formattedNewValue = `${prefix}${number}`
-  if (`${newValue} ` === previousValue) {
-    return newValue.slice(0, -2)
-  } else {
-    return IBAN.printFormat(formattedNewValue)
-  }
+  return `${newValue} ` === previousValue
+    ? newValue.slice(0, -2)
+    : IBAN.printFormat(formattedNewValue)
 }
 
 export const validateIbanFormat = (value: string): boolean => {
