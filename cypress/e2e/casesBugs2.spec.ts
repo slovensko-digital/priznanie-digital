0a28ed4782ad449de2ec408885ff1f47e69dfca7
Marek Vigaš <vigas147@gmail.com>
2025-04-26 16:02:29 +0200

    split bugs case

---
+++ cypress/e2e/casesBugs2.spec.ts
@@ -0,0 +1,18 @@
+import { executeAllTestCases } from './executeCase'
+
+describe('Cases', () => {
+  executeAllTestCases([
+    'bugReport14',
+    'bugReport15',
+    'bugReport16',
+    'bugReport17',
+    'bugReport18',
+    'bugReport19',
+    'bugReport20',
+    'bugReport21',
+    'bugReport22',
+    'bugReport23',
+    'bugReport24',
+    'employmentWrongChildBonus',
+  ])
+})
