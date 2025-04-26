69db4100999433b9be063c2bf197fb6c0093c8d9
Marek Vigaš <vigas147@gmail.com>
2025-04-26 20:22:53 +0200

    fix merge

--- src/types/E2eTestUserInput.ts
+++ src/types/E2eTestUserInput.ts
@@ -1,7 +1,6 @@
 import { TaxFormUserInput } from './TaxFormUserInput'
 
 export interface E2eTestUserInput extends TaxFormUserInput {
-  uroky_nesplna_datumy?: boolean
   expectNgoDonationValue?: boolean
   percent2?: string
   percent3?: string
