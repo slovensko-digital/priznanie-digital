696531f1a7b433903f1b11f0e264fee6bbbe7aea
Marek Vigaš <vigas147@gmail.com>
2026-01-16 19:50:13 +0100

    Updates wording for clarity and accuracy (#1049)
    Clarifies the condition for claiming 2% tax for parents, specifying that they must be of retirement age.
    
    Removes an outdated item from the tax form section to ensure accurate guidance.

--- src/pages/dve-percenta-rodicom.tsx
+++ src/pages/dve-percenta-rodicom.tsx
@@ -102,7 +102,7 @@ const DvePercentaRodicom: Page<DvePercentaRodicomUserInput> = ({
             <ErrorSummary<DvePercentaRodicomUserInput> errors={props.errors} />
             <Form className="form" noValidate>
               <Fieldset
-                title={`Chcete poukázať 2% dane rodičom?`}
+                title={`Chcete poukázať 2% dane rodičom, ktorí dovŕšili dôchodkový vek?`}
                 error={props.errors.dve_percenta_rodicom}
                 hint={`Môžete poukázať ${formatCurrency(calculatedTax.suma_2_percenta.toNumber())}, jednému alebo obidvom rodičom. Poukázanie 2% dane rodičom neovplyvňuje možnosť poukázať 2% alebo 3% dane neziskovej organizácii.`}
               >
