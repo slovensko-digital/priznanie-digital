3c80e7bdfabdfe7ea108f33baecb1a7c10e0aca6
Marek Vigaš <vigas147@gmail.com>
2026-02-17 20:02:12 +0100

    add tests

--- __tests__/calcIntegration.test.ts
+++ __tests__/calcIntegration.test.ts
@@ -90,3 +90,49 @@ describe('vydavkyPar6ods10_ods1a2 field', () => {
     expect(jsonForm.dokument.telo.vydavkyPar6ods10_ods1a2).toBe('0')
   })
 })
+
+describe('r146 and r146a fields in XML', () => {
+  test('should be filled when hasChildren is "yes"', async () => {
+    const inputModule = await import('./testCases/withChildrenInput')
+    const input: TaxFormUserInput = {
+      ...inputModule.withChildrenInput,
+      hasChildren: 'yes',
+    }
+
+    const taxForm = calculate(setDate(input, new Date(2024, 1, 10)))
+    const jsonForm = convertToJson(taxForm)
+
+    expect(jsonForm.dokument.telo.r146).toBeDefined()
+    expect(jsonForm.dokument.telo.r146a).toBeDefined()
+  })
+
+  test('should be filled when hasChildren is "income-used-by-someone-else"', async () => {
+    const inputModule = await import('./testCases/withChildrenInput')
+    const input: TaxFormUserInput = {
+      ...inputModule.withChildrenInput,
+      hasChildren: 'income-used-by-someone-else',
+      children: [],
+    }
+
+    const taxForm = calculate(setDate(input, new Date(2024, 1, 10)))
+    const jsonForm = convertToJson(taxForm)
+
+    expect(jsonForm.dokument.telo.r146).toBeDefined()
+    expect(jsonForm.dokument.telo.r146a).toBeDefined()
+  })
+
+  test('should be empty when hasChildren is "no"', async () => {
+    const inputModule = await import('./testCases/withChildrenInput')
+    const input: TaxFormUserInput = {
+      ...inputModule.withChildrenInput,
+      hasChildren: 'no',
+      children: [],
+    }
+
+    const taxForm = calculate(setDate(input, new Date(2024, 1, 10)))
+    const jsonForm = convertToJson(taxForm)
+
+    expect(jsonForm.dokument.telo.r146).toBe('')
+    expect(jsonForm.dokument.telo.r146a).toBe('')
+  })
+})
