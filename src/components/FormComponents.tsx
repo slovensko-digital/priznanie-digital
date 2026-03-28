962346419f6191f5cea771b762cb6805eb096180
Marek Vigaš <vigas147@gmail.com>
2026-03-28 18:48:07 +0100

    adjust hints

--- src/components/FormComponents.tsx
+++ src/components/FormComponents.tsx
@@ -106,7 +106,9 @@ export const Input = <Name extends UserInputFieldName>({
       >
         {label}
       </label>
-      <span className="govuk-hint">{hint}</span>
+      <span className="govuk-hint" id={`${props.name}-hint`}>
+        {hint}
+      </span>
       {meta.error ? (
         <span
           id={`${props.name}-error`}
@@ -126,6 +128,7 @@ export const Input = <Name extends UserInputFieldName>({
           'aria-invalid': true,
           'aria-describedby': `${props.name}-error`,
         })}
+        aria-details={hint ? `${props.name}-hint` : undefined}
         {...getNumberInputProps()}
         {...field}
         {...props}
