56e7d2aefb46fe1d6f755ab36621aa4c60cfa4e2
Tomáš Drga <tomasxdrga@gmail.com>
2026-07-02 12:02:56 +0200

    Add /up health check page, fix status-check response, add CODEOWNERS

--- src/pages/api/status-check.ts
+++ src/pages/api/status-check.ts
@@ -3,9 +3,9 @@ import { RollbarInstance } from '../../lib/rollbar'
 
 export default async (_req: NextApiRequest, res: NextApiResponse) => {
   try {
-    res.status(200)
+    res.status(200).end()
   } catch (_error) {
     RollbarInstance.error(_error)
-    res.status(500)
+    res.status(500).end()
   }
 }
